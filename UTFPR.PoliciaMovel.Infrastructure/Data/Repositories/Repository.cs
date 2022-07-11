938d559332775355357cfb7879d7cf2094995f6d
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2022-07-11 15:55:07 -0300

    Adicionar arquivos de projeto.

---
+++ UTFPR.PoliciaMovel.Infrastructure/Data/Repositories/Repository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using UTFPR.PoliciaMovel.Application.Interfaces.Repositories;
+
+namespace UTFPR.PoliciaMovel.Infrastructure.Data.Repositories
+{
+    public abstract class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly IMongoCollection<T> _collection;
+
+        public Repository(IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetValue<string>("ConnectionString"));
+
+            var mondoDatabase = mongoClient.GetDatabase(configuration.GetValue<string>("Database"));
+
+            _collection = mondoDatabase.GetCollection<T>(typeof(T).Name);
+        }
+
+        public async Task Save(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+    }
+}
