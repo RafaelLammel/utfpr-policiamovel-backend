0abb800d217e485463ee866e852cceedfc709cca
unknown <kelvinmartins999@gmail.com>
2022-10-16 14:40:21 -0300

    Terminando a adição do LastPutDate

--- UTFPR.PoliciaMovel.Application/Locations/GetLocationsRequest.cs
+++ UTFPR.PoliciaMovel.Application/Locations/GetLocationsRequest.cs
@@ -5,7 +5,9 @@ namespace UTFPR.PoliciaMovel.Application.Locations
     public class GetLocationsRequest
     {
         public string UserId { get; set; }
+        public string Login { get; set; }
         public string Longitude { get; set; }
         public string Latitude { get; set; }
+        public DateTime LastPutDate { get; set; }
     }
 }
